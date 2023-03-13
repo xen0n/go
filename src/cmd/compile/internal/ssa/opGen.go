c49f00d0880df3aad471293e3706b2f7fb2a2fb4
WANG Xuerui <git@xen0n.name>
2023-03-13 14:28:25 +0800

    cmd/compile: allow loong64 CALL* ops to take variable number of args
    Similar to CL 360216, in preparation of regabi support.
    
    Change-Id: I4a67ba118b1a8c571706cf133204198bd263d20e

--- src/cmd/compile/internal/ssa/opGen.go
+++ src/cmd/compile/internal/ssa/opGen.go
@@ -24176,7 +24176,7 @@ var opcodeTable = [...]opInfo{
 	{
 		name:         "CALLstatic",
 		auxType:      auxCallOff,
-		argLen:       1,
+		argLen:       -1,
 		clobberFlags: true,
 		call:         true,
 		reg: regInfo{
@@ -24186,7 +24186,7 @@ var opcodeTable = [...]opInfo{
 	{
 		name:         "CALLtail",
 		auxType:      auxCallOff,
-		argLen:       1,
+		argLen:       -1,
 		clobberFlags: true,
 		call:         true,
 		tailCall:     true,
@@ -24197,7 +24197,7 @@ var opcodeTable = [...]opInfo{
 	{
 		name:         "CALLclosure",
 		auxType:      auxCallOff,
-		argLen:       3,
+		argLen:       -1,
 		clobberFlags: true,
 		call:         true,
 		reg: regInfo{
@@ -24211,7 +24211,7 @@ var opcodeTable = [...]opInfo{
 	{
 		name:         "CALLinter",
 		auxType:      auxCallOff,
-		argLen:       2,
+		argLen:       -1,
 		clobberFlags: true,
 		call:         true,
 		reg: regInfo{
